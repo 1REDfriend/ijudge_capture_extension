f44e1faaad76d31c427a7308378c37aa015069f3
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-08-28 23:46:29 +0700

    create change theme ijudge

--- ijudge capture/script/background.js
+++ ijudge capture/script/background.js
@@ -11,27 +11,14 @@ async function processProblemLinks(problemLinks) {
                 const currentTabId = tabs[0].id;
                 console.log(`Navigating to: ${link} in tab ID: ${currentTabId}`);
 
-                // Update the current tab with the new URL
                 chrome.tabs.update(currentTabId, { url: link }, () => {
-                    console.log(`Tab updated with URL: ${link}`);
                     chrome.tabs.onUpdated.addListener(async function listener(tabId, info) {
                         if (tabId === currentTabId && info.status === 'complete') {
-                            console.log(`Tab ID ${tabId} completed loading.`);
-
-                            // Add a slight delay to ensure content script is loaded
-                            await new Promise(resolve => setTimeout(resolve, 1000));
-                            setTimeout(() => {
-                                chrome.tabs.sendMessage(tabId, { action: 'cheat' }, (response) => {
-                                    if (chrome.runtime.lastError) {
-                                        console.error("Error sending message to content script:", chrome.runtime.lastError.message);
-                                        reject(new Error(chrome.runtime.lastError.message));
-                                    } else {
-                                        console.log("Response received from content script:", response);
-                                        resolve(response); // Resolve when content script sends a response
-                                    }
-                                });
-                            }, 1000); // 1 second delay to ensure content script is ready
-
+                            chrome.tabs.executeScript({
+                                target: tabId,
+                                func: ["script/cheat.js"]
+                            })
+                            await new Promise(resolve => setTimeout(resolve, 3000))
                             chrome.tabs.onUpdated.removeListener(listener);
                         }
                     });
@@ -57,6 +44,62 @@ chrome.storage.onChanged.addListener(async (changes, areaName) => {
             }
             // Optional delay after processing all links
             await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+    }
+    if (areaName === 'local' && changes.theme != null) {
+        const value = await changes.theme.oldValue;
+        if (value == 'light') {
+            await chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+                if (await tabs.length > 0) {
+                    await chrome.scripting.executeScript({
+                        target: { tabId: tabs[0].id },
+                        func: async() => {
+                            const bg_elements = document.querySelector("body");
+                            const bg2 = document.querySelectorAll("nav, footer");
+                            const tables_bg = document.querySelectorAll('.MuiPaper-root')
+                            const fonts = document.querySelectorAll("p,h1,h2,h3,h4,h5,.MuiTableCell-root,.MuiAlert-message,.MuiChip-label,.MuiButton-text")
+
+                            tables_bg.forEach(element => {
+                                element.style.backgroundColor = "#f3f4f6"
+                            })
+                            bg_elements.style.backgroundColor = "#f3f4f6"
+                            bg2.forEach(element => {
+                                element.style.backgroundColor = "#f3f4f6";
+                                element.style.borderColor = "#ffffff";
+                            });
+                            fonts.forEach(element => {
+                                element.style.color = "#000000";
+                            })
+                        }
+                    })
+                }
+            })
+        } else if (value == "dark") {
+            await chrome.tabs.query({ active: true, currentWindow: true }, async(tabs) => {
+                if (await tabs.length > 0) {
+                    await chrome.scripting.executeScript({
+                        target: { tabId: tabs[0].id },
+                        func: async() => {
+                            const bg_elements = document.querySelector("body");
+                            const bg2 = document.querySelectorAll("nav, footer");
+                            const tables_bg = document.querySelectorAll('.MuiPaper-root')
+                            const fonts = document.querySelectorAll("p,h1,h2,h3,h4,h5,.MuiTableCell-root,.MuiAlert-message,.MuiChip-label,.MuiButton-text")
+
+                            tables_bg.forEach(element => {
+                                element.style.backgroundColor = "#141d2e"
+                            })
+                            bg_elements.style.backgroundColor = "#0d1016"
+                            bg2.forEach(element => {
+                                element.style.backgroundColor = "#141d2e";
+                                element.style.borderColor = "#1d4eb1";
+                            });
+                            fonts.forEach(element => {
+                                element.style.color = "#ffffff";
+                            })
+                        }
+                    })
+                }
+            })
         }
     }
 });
